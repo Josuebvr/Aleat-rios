0f10e579f5fda07357b1583cbee5e06409813a25
Josué <josuebvrios@gmail.com>
2022-10-26 11:14:59 -0300

    Colisão com as bordas

--- pong.js
+++ pong.js
@@ -1,9 +1,10 @@
 
-let xbolinha = 300; //posição horizontal da bolinha
-let ybolinha = 200; //posição vertical da bolinha
+let xbolinha = 300;   //posição horizontal da bolinha
+let ybolinha = 200;  //posição vertical da bolinha
 let diametro = 15;  //diametro da bolinha
-let velocidadeXbolinha = 5; //velocidade horizontal da bolinha
-let velocidadeYbolinha = 5; //velocidade vertical da bolinha
+let velocidadeXbolinha = 6;  //velocidade horizontal da bolinha
+let velocidadeYbolinha = 6; //velocidade vertical da bolinha
+let raio = diametro / 2;   //raio da bolinha
 
 
 
@@ -12,14 +13,19 @@ function setup() {
 }
 
 
+function draw() {  //função de desenho
+  background(0);  //cor de fundo
+  circle(xbolinha, ybolinha, diametro); //desenha a bolinha
+  xbolinha += velocidadeXbolinha;      //movimenta a bolinha na horizontal
+  ybolinha += velocidadeYbolinha;     //movimenta a bolinha na vertical
 
+  if (xbolinha + raio > width || xbolinha - raio < 0) { //se a bolinha chegar no limite da tela
+    velocidadeXbolinha *= -1;                   //inverte a velocidade horizontal
+  }
+  if (ybolinha + raio > height || ybolinha - raio < 0)  // se a bolinha chegar no limite da tela
+    velocidadeYbolinha *= -1;                   //inverte a velocidade vertical
+  }
 
-function draw() {
-  background(0);
-  circle(xbolinha, ybolinha, diametro);
-  xbolinha += velocidadeXbolinha;
-  ybolinha += velocidadeYbolinha;
-}
 
 
 
